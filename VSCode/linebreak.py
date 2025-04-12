3f69d4da37f67c4b1e58ea14f18cf4367e180334
Neko-nos <Neko-nos@users.noreply.github.com>
2025-04-12 19:19:16 +0900

    refactor: make helper functions and config classes

--- VSCode/linebreak.py
+++ VSCode/linebreak.py
@@ -1,55 +1,150 @@
 import argparse
-import re
+from dataclasses import dataclass
 from pathlib import Path
+import re
+from typing import Literal
 
-# HTMLのtagを用いているものは簡単なものしか自分は使わないので複雑なものは割愛
+
+# HTMLをMarkdownで使うことはあまりないので割愛
 # ref: https://qiita.com/Qiita/items/c686397e4a0f4f11683d
 # ref: https://docs.github.com/ja/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
-tab_pattern = re.compile(r"\t")
-br_pattern = re.compile(r"<br>$")
-comment_pattern = re.compile(r"<!-- .* -->")
-code_pattern = re.compile(r"```")
-title_pattern = re.compile(r"#+ ")
-alert_pattern = re.compile(r"> \[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]")
-quote_pattern = re.compile(r"(> )+")
-image_pattern = re.compile(r"(!\[.*\]\(.+\))|(<img .* src=.+>)")
-table_pattern = re.compile(r"((\|:-+:\|)+)|((\|:-+\|-+:\|)+\|:-+:\|)")
-table_cell_pattern = re.compile(r"\| .* \|")
-# ネストも考慮する
-list_pattern = re.compile(r"(\s*((\*|\+|-) )|(\d+\. ))")
-# lists用の最小のindent幅は3なので、それ以下は通常のテキストとみなす(実際に、日本語で段落の始まりのスペースも2で収まる)
-# indent幅でより正確に決められるが複雑なのでUser側に任せる
-list_text_pattern = re.compile(r" {3,}.*")
-# listsの中にさらに特殊記号がある場合にも対応する
-# (ちなみにre.searchはテキスト内の記号とマッチしてしまう可能性があるので面倒でもこっちのほうが良い)
-list_comment_pattern = re.compile(r" {3,}<!-- .* -->")
-list_code_pattern = re.compile(r" {3,}```")
-list_title_pattern = re.compile(r" {3,}#+")
-list_quote_pattern = re.compile(r" {3,}(> )+")
-list_image_pattern = re.compile(r" {3,}(!\[.*\]\(.+\))|(<img .* src=.+>)")
-list_table_pattern = re.compile(r" {3,}((\|:-+:\|)+)|((\|:-+\|-+:\|)+\|:-+:\|)")
-list_table_cell_pattern = re.compile(r" {3,}\| .* \|")
-list_indent_pattern = re.compile(r" {3,}")
+@dataclass
+class Patterns:
+    # 先頭のtab以外はインデント処理に関係ないので無視する
+    tab: re.Pattern = re.compile(r"^\t")
+    br_tag: re.Pattern = re.compile(r"<br>$")
+    comment: re.Pattern = re.compile(r"<!-- .* -->")
+    code: re.Pattern = re.compile(r"```")
+    title: re.Pattern = re.compile(r"#+ ")
+    alert: re.Pattern = re.compile(r"> \[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]")
+    quote: re.Pattern = re.compile(r"(> )+")
+    image: re.Pattern = re.compile(r"(!\[.*\]\(.+\))|(<img .* src=.+>)")
+    table: re.Pattern = re.compile(r"((\|:-+:\|)+)|((\|:-+\|-+:\|)+\|:-+:\|)")
+    table_cell = re.compile(r"\| .* \|")
+    list_item: re.Pattern = re.compile(r"\s*(\*|\+|-|(\d+\.)) ")
+    # ネストに必要なindentでのwhitespaceの最小値は3
+    list_indent_prefix: re.Pattern = re.compile(r"^( {3,})")
 
 
-def main(filepath):
-    filepath = Path(filepath)
-    if filepath.suffix != ".md":
-        raise ValueError("`filepath` must be the path of a markdown file.")
+@dataclass
+class Status:
+    is_code: bool = False
+    is_alert: bool = False
+    is_table: bool = False
+    is_list: bool = False
 
-    with open(filepath, mode="r", encoding="utf-8") as f:
-        lines = f.read().splitlines()
+    def show_statuses(self):
+        if self.is_list:
+            if self.is_code:
+                print("list and code")
+            elif self.is_alert:
+                print("list and alert")
+            elif self.is_table:
+                print("list and table")
+            else:
+                print("list")
+        elif self.is_code:
+            print("code")
+        elif self.is_alert:
+            print("alert")
+        elif self.is_table:
+            print("table")
+        else:
+            print("normal")
 
-    # <br>を入れるかを決めていく
+    def reset_block(self):
+        # listだけは他のブロックと共存できるのでFalseにしない
+        self.is_code = False
+        self.is_alert = False
+        self.is_table = False
+
+    def update(
+        self,
+        status: Literal["code", "alert", "table", "list"],
+        unset_list: bool = False,
+    ):
+        self.reset_block()
+        setattr(self, f"is_{status}", True)
+        if unset_list:
+            self.is_list = False
+
+
+def get_newline_suffix(
+    status: Status,
+    patterns: Patterns,
+    line: str,
+    next_line: str,
+    unset_list: bool,
+    is_next_line_list: bool = False,
+) -> Literal["", "\n", "\n\n", "<br>\n"]:
+    # 複数の空行が連続している時、<br>と\n\nによる改行が重複するので\n\nの方を無くす
+    if not line and not next_line:
+        return ""
+    elif not line or not next_line:
+        return "\n"
+    elif patterns.comment.match(line) or patterns.comment.match(next_line):
+        return "\n"
+    elif patterns.title.match(line) or patterns.title.match(next_line):
+        return "\n"
+    elif patterns.image.match(line) or patterns.image.match(next_line):
+        return "\n"
+    elif patterns.code.match(line):
+        status.update("code", unset_list=unset_list)
+        return "\n"
+    elif patterns.alert.match(line):
+        status.update("alert", unset_list=unset_list)
+        return "\n"
+    elif patterns.table_cell.match(line) and patterns.table_cell.match(next_line):
+        status.update("table", unset_list=unset_list)
+        return "\n"
+    elif patterns.quote.match(line) and patterns.quote.match(next_line):
+        return "\n"
+    elif patterns.quote.match(line):
+        return "\n\n"
+    # alert blockは他の要素内に作ることができないのでここでは考慮しなくて良い (GitHub Markdown)
+    elif patterns.list_item.match(line) and (
+        patterns.list_item.match(next_line)
+        or patterns.comment.match(next_line)
+        or patterns.code.match(next_line)
+        or patterns.title.match(next_line)
+        or patterns.quote.match(next_line)
+        or patterns.image.match(next_line)
+    ):
+        # listに設定する
+        status.update("list", unset_list=False)
+        return "\n"
+    # blockを作るものでは無いので通常のテキストが続いていると考えられる
+    # ここで、status.is_listの中でこの関数を使う時には文頭にあるindentをなくしてから渡しているので正規表現ではなく引数からlistの終了を予測する必要がある
+    elif patterns.list_item.match(line) and is_next_line_list:
+        status.update("list", unset_list=False)
+        return "<br>" + "\n"
+    # 一番外側のlistが終了している
+    elif status.is_list and not is_next_line_list:
+        # 全てFalseにする
+        status.update("list", unset_list=True)
+        return "\n"
+    # lineは通常のテキスト
+    elif (
+        patterns.code.match(next_line)
+        or patterns.title.match(next_line)
+        or patterns.alert.match(next_line)
+        or patterns.quote.match(next_line)
+        or patterns.image.match(next_line)
+        or patterns.table.match(next_line)
+    ):
+        return "\n"
+    else:
+        return "<br>" + "\n"
+
+
+def process_markdown(lines: list[str]) -> list[str]:
+    """Processes a list of Markdown lines and adds <br> tags where needed."""
+    patterns = Patterns()
+    status = Status()
     new_lines = []
-    num_lines = len(lines)
-    is_code = False
-    is_alert = False
-    is_table = False
-    is_list = False
     for idx, line in enumerate(lines):
-        if idx == (num_lines - 1):
-            # 一般的にはfileの最後の行は空行になっているがそうでない場合も考慮し、書き込み時にも空行にしておく
+        # 最後の行は基本的には空行になっているが、そうでない場合は代わりに挿入しておく
+        if idx == len(lines) - 1:
             if line:
                 next_line = ""
             else:
@@ -58,166 +153,107 @@ def main(filepath):
         else:
             next_line = lines[idx + 1]
 
-        # 既に末尾に<br>等があると重なってしまうので削っておく
-        # 2つ半角スペースがあるのはtrailling spaces拡張機能との相性が良くない
-        line = re.sub(br_pattern, "", line)
-        line = line.rstrip()
-        next_line = re.sub(br_pattern, "", next_line)
-        next_line = next_line.rstrip()
-        # 先頭にtabがある場合にlistsのネストの処理が面倒なのでspaceに変換させる
-        line = re.sub(tab_pattern, "    ", line)
-        next_line = re.sub(tab_pattern, "    ", next_line)
-        # 特殊な段落を作るものを処理
-        if is_code:
+        # 最初の改行に使われる記号等を削除する
+        line = patterns.br_tag.sub("", line).rstrip()
+        next_line = patterns.br_tag.sub("", next_line).rstrip()
+        # indentはwhitespaceで数えているのでtabも変換しておく
+        line = patterns.tab.sub(" " * 4, line)
+        next_line = patterns.tab.sub(" " * 4, next_line)
+
+        # 特別なブロックを作るものをまずは処理する
+        if status.is_code:
             new_lines.append(line + "\n")
-            end = re.sub(list_code_pattern, "", line) if is_list else re.sub(code_pattern, "", line)
-            if not end:
-                is_code = False
-            continue
-        # alert blockは他の要素内に作ることができない(GitHub Markdown)
-        elif is_alert:
-            # alertの目印となる部分はis_alertの判定に使っていて、この段階ではalert内の要素のみを扱うことになる
-            # その為、next_resultと違って、現在の行がquote_patternにmatchするかを判定する必要はない(確実にmatchする)
-            next_result = quote_pattern.match(next_line)
-            if next_result:
-                new_lines.append(line + "<br>" + "\n")
-                continue
+            if status.is_list:
+                # indentの部分を取り除いて考えれば通常の場合の正規表現を使い回せる
+                line_no_indent = patterns.list_indent_prefix.sub("", line)
+                is_code_end = patterns.code.match(line_no_indent) is not None
             else:
+                is_code_end = patterns.code.match(line) is not None
+            if is_code_end:
+                status.is_code = False
+        # alert blockは他の要素内に作ることができない (GitHub Markdown)
+        elif status.is_alert:
+            # alertの目印となる部分はis_alertの判定に使っていて、この段階ではalert内の要素のみを扱うことになる
+            # その為、is_alert_endと違って、現在の行がquote_patternにmatchするかを判定する必要はない(確実にmatchする)
+            is_alert_end = patterns.quote.match(next_line) is None
+            if is_alert_end:
                 new_lines.append(line + "\n")
-                # 既に後ろに空行が来ていたら追加しない
+                # 既に後ろに空行が来ていたら追加の\nを追加しなくても切り替わる
                 if next_line:
                     # <br>ではblockが切り替わらない
                     new_lines.append("\n")
-                is_alert = False
-                continue
-        elif is_table:
+                status.is_alert = False
+            else:
+                new_lines.append(line + "<br>" + "\n")
+        elif status.is_table:
             # is_tableになって1回目(つまり2行目)のみtable_patternに該当する
-            current_result = (
-                (list_table_cell_pattern.match(line) or list_table_pattern.match(line))
-                if is_list
-                else (table_cell_pattern.match(line) or table_pattern.match(line))
-            )
-            next_result = list_table_cell_pattern.match(next_line) if is_list else table_cell_pattern.match(next_line)
-            if current_result and next_result:
+            if status.is_list:
+                line_no_indent = patterns.list_indent_prefix.sub("", line)
+                next_line_no_indent = patterns.list_indent_prefix.sub("", next_line)
+                is_table_current = patterns.table.match(
+                    line_no_indent
+                ) or patterns.table_cell.match(line_no_indent)
+                is_table_next = patterns.table_cell.match(next_line_no_indent)
+            else:
+                is_table_current = patterns.table.match(
+                    line
+                ) or patterns.table_cell.match(line)
+                is_table_next = patterns.table_cell.match(next_line)
+
+            if is_table_current and is_table_next:
                 new_lines.append(line + "\n")
-                continue
-            elif current_result:
+            elif is_table_current:
                 new_lines.append(line + "\n")
                 # <br>では段落が分けられない
                 new_lines.append("\n")
-                continue
-        elif is_list:
-            # indentが必要なだけで基本的な処理は後ろの処理とあまり変わらない
-            if list_comment_pattern.match(line) or list_comment_pattern.match(next_line):
-                new_lines.append(line + "\n")
-            elif list_title_pattern.match(line) or list_title_pattern.match(next_line):
-                new_lines.append(line + "\n")
-            elif list_image_pattern.match(line) or list_image_pattern.match(next_line):
-                new_lines.append(line + "\n")
-            elif list_code_pattern.match(line):
-                new_lines.append(line + "\n")
-                is_code = True
-            elif alert_pattern.match(line):
-                new_lines.append(line + "\n")
-                is_alert = True
-            elif list_table_cell_pattern.match(line) and list_table_pattern.match(next_line):
-                new_lines.append(line + "\n")
-                is_table = True
-            elif list_quote_pattern.match(line) and list_quote_pattern.match(next_line):
-                new_lines.append(line + "\n")
-            elif list_quote_pattern.match(line):
-                new_lines.append(line + "\n")
-                new_lines.append("\n")
-            elif list_text_pattern.match(line) and list_text_pattern.match(next_line):
-                new_lines.append(line + "<br>" + "\n")
-            elif list_text_pattern.match(line) and (
-                list_pattern.match(next_line)
-                or list_comment_pattern.match(next_line)
-                or list_code_pattern.match(next_line)
-                or list_title_pattern.match(next_line)
-                or list_quote_pattern.match(next_line)
-                or list_image_pattern.match(next_line)
-            ):
-                new_lines.append(line + "\n")
-            elif not list_indent_pattern.match(next_line):
-                new_lines.append(line + "\n")
-                is_list = False
-            continue
-
-        # 空行に関しては何もしない
-        # 複数の空行が連続していたら一つにする様にする
-        if not line and not next_line:
-            continue
-        elif not line or not next_line:
-            new_lines.append(line + "\n")
-        elif comment_pattern.match(line) or comment_pattern.match(next_line):
-            new_lines.append(line + "\n")
-        elif title_pattern.match(line) or title_pattern.match(next_line):
-            new_lines.append(line + "\n")
-        elif image_pattern.match(line) or image_pattern.match(next_line):
-            new_lines.append(line + "\n")
-        elif code_pattern.match(line):
-            new_lines.append(line + "\n")
-            is_code = True
-            is_alert = False
-            is_table = False
-            is_list = False
-        elif alert_pattern.match(line):
-            new_lines.append(line + "\n")
-            is_alert = True
-            is_code = False
-            is_table = False
-            is_list = False
-        elif table_cell_pattern.match(line) and table_pattern.match(next_line):
-            new_lines.append(line + "\n")
-            is_table = True
-            is_code = False
-            is_alert = False
-            is_list = False
-        elif quote_pattern.match(line) and quote_pattern.match(next_line):
-            new_lines.append(line + "\n")
-        elif quote_pattern.match(line):
-            new_lines.append(line + "\n")
-            new_lines.append("\n")
-        # listsの処理
-        elif list_pattern.match(line) and (
-            list_pattern.match(next_line)
-            or list_comment_pattern.match(next_line)
-            or list_code_pattern.match(next_line)
-            or list_title_pattern.match(next_line)
-            or list_quote_pattern.match(next_line)
-            or list_image_pattern.match(next_line)
-        ):
-            new_lines.append(line + "\n")
-            is_list = True
-        elif list_pattern.match(line) and list_text_pattern.match(next_line):
-            new_lines.append(line + "<br>" + "\n")
-            is_list = True
-        # indentがない場合に、基本的にlists一行で終わらせることはないので
-        # その後もlistsのblockにあるとみなして考える
-        elif list_pattern.match(line):
-            new_lines.append(line + "<br>" + "\n")
-        elif list_pattern.match(next_line):
-            new_lines.append(line + "\n")
-            new_lines.append("\n")
-        elif (
-            code_pattern.match(next_line)
-            or title_pattern.match(next_line)
-            or alert_pattern.match(next_line)
-            or quote_pattern.match(next_line)
-            or image_pattern.match(next_line)
-            or table_cell_pattern.match(next_line)
-        ):
-            new_lines.append(line + "\n")
         else:
-            new_lines.append(line + "<br>" + "\n")
+            line_no_indent = patterns.list_indent_prefix.sub("", line)
+            next_line_no_indent = patterns.list_indent_prefix.sub("", next_line)
+            is_next_line_list = (len(next_line) - len(next_line_no_indent)) >= 3
+            new_line = line + get_newline_suffix(
+                status,
+                patterns,
+                line,
+                next_line,
+                unset_list=not status.is_list,
+                is_next_line_list=is_next_line_list,
+            )
+            new_lines.append(new_line)
 
-    with open(filepath, mode="w", encoding="utf-8") as f:
-        f.writelines(new_lines)
+    return new_lines
+
+
+def main(filepath: str | Path):
+    filepath = Path(filepath)
+    if filepath.suffix != ".md":
+        raise ValueError("`filepath` must be the path of a markdown file.")
+
+    try:
+        with open(filepath, mode="r", encoding="utf-8") as f:
+            lines = f.read().splitlines()
+    except FileNotFoundError:
+        print(f"Error: File not found at {filepath}")
+        return
+    except Exception as e:
+        print(f"Error reading file {filepath}: {e}")
+        return
+
+    new_content_lines = process_markdown(lines)
+
+    try:
+        with open(filepath, mode="w", encoding="utf-8", newline="\n") as f:
+            f.writelines(new_content_lines)
+        print(f"Successfully processed {filepath}")
+    except Exception as e:
+        print(f"Error writing file {filepath}: {e}")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", required=True, type=str, help="The path of the markdown file.")
-    args = parser.parse_args()
-    main(args.file)
+    parser = argparse.ArgumentParser(
+        description="Adds <br> tags to Markdown paragraphs for explicit line breaks."
+    )
+    parser.add_argument(
+        "-f", "--file", required=True, type=str, help="The path of the markdown file."
+    )
+    #args = parser.parse_args()
+    main("/Users/yoshihiratakahide/configs/VSCode/debug.md")
