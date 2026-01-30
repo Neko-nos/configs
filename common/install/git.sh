c0621f64cbe641203d46c2dbc1440b0117c271c6
Neko-nos <Neko-nos@users.noreply.github.com>
2026-01-31 02:02:19 +0900

    feat: make a symlink of git/.gitignore_template for global gitignore.

--- common/install/git.sh
+++ common/install/git.sh
@@ -3,6 +3,9 @@
 # Stop running this script if any error occurs
 set -e
 
+script_dir="${${(%):-%N}:A:h}"
+common_gitdir="${script_dir}/../git"
+
 #######################################
 # Set up ~/.gitconfig with user info and a shared include.
 # Globals:
@@ -22,8 +25,7 @@ function __set_up_gitconfig {
     echo "    email = ${email}" >> ~/.gitconfig
     echo "    name = ${name}" >> ~/.gitconfig
     echo '[include]' >> ~/.gitconfig
-    local script_dir="${${(%):-%N}:A:h}"
-    local common_gitconfig="${script_dir}/../git/.gitconfig"
+    local common_gitconfig="${common_gitdir:A}/.gitconfig"
     echo "    path = ${common_gitconfig:A}" >> ~/.gitconfig
 }
 
@@ -31,7 +33,7 @@ if [[ -f ~/.gitconfig ]]; then
     echo 'You have already created .gitconfig'
     printf 'Do you want to include our .gitconfig? [y/N]: '
     if read -q; then
-        local timestamp="$(date +%Y%m%d%H%M%S)"
+        timestamp="$(date +%Y%m%d%H%M%S)"
         # Print a newline using echo because read -q doesn't.
         echo; mv ~/.gitconfig ~/.gitconfig_old_"${timestamp}"
         echo "Renamed your .gitconfig to .gitconfig_old_${timestamp} as a backup file."
@@ -41,6 +43,23 @@ if [[ -f ~/.gitconfig ]]; then
     fi
 else
     echo; __set_up_gitconfig
+fi
+
+if [[ -f "${XDG_CONFIG_HOME:-$HOME/.config}/git/ignore" ]]; then
+    echo 'You have already created global git ignore file.'
+    printf 'Do you want to replace it with our global git ignore file? [y/N]: '
+    if read -q; then
+        timestamp="$(date +%Y%m%d%H%M%S)"
+        # Print a newline using echo because read -q doesn't.
+        echo; mv "${XDG_CONFIG_HOME:-$HOME/.config}/git/ignore" "${XDG_CONFIG_HOME:-$HOME/.config}/git/ignore_old_${timestamp}"
+        echo "Renamed your global git ignore file to ignore_old_${timestamp} as a backup file."
+        ln -s "${common_gitdir:A}/.gitignore_template" "${XDG_CONFIG_HOME:-$HOME/.config}/git/ignore"
+    else
+        echo
+    fi
+else
+    mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/git"
+    ln -s "${common_gitdir:A}/.gitignore_template" "${XDG_CONFIG_HOME:-$HOME/.config}/git/ignore"
 fi
 
 echo 'Finished git configuration!'
