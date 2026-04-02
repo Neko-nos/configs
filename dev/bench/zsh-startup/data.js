25f6c833a9bc817a9dffafc0b106481a6a9aeed3
github-action-benchmark <github@users.noreply.github.com>
2026-04-02 09:22:39 +0000

    add zsh startup benchmark (customSmallerIsBetter) benchmark result for 3701435426d5c593d008602c6db791fc4ed8e593

--- dev/bench/zsh-startup/data.js
+++ dev/bench/zsh-startup/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774107821818,
+  "lastUpdate": 1775121759294,
   "repoUrl": "https://github.com/Neko-nos/configs",
   "entries": {
     "zsh startup benchmark": [
@@ -35,6 +35,42 @@ window.BENCHMARK_DATA = {
             "name": "zsh average startup time",
             "value": 1131.18918,
             "range": "± 6.551703",
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "158806384+Neko-nos@users.noreply.github.com",
+            "name": "Neko-nos",
+            "username": "Neko-nos"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3701435426d5c593d008602c6db791fc4ed8e593",
+          "message": "Merge pull request #44 from Neko-nos/dependabot/github_actions/astral-sh/setup-uv-8.0.0\n\nchore(deps): bump astral-sh/setup-uv from 7.6.0 to 8.0.0",
+          "timestamp": "2026-04-02T18:21:07+09:00",
+          "tree_id": "88f18b10ba793d75e483a1359df149dac7fb7610",
+          "url": "https://github.com/Neko-nos/configs/commit/3701435426d5c593d008602c6db791fc4ed8e593"
+        },
+        "date": 1775121758643,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh initial startup time",
+            "value": 1092.688283,
+            "range": "± 378.393883",
+            "unit": "ms"
+          },
+          {
+            "name": "zsh average startup time",
+            "value": 999.988115,
+            "range": "± 8.673787",
             "unit": "ms"
           }
         ]
