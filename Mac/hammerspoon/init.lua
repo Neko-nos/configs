3dea2c453e19afe14e3fc29886d14f5a5f7bc7d6
Neko-nos <Neko-nos@users.noreply.github.com>
2026-09-21 17:05:48 +0900

    feat(vscode): route Cmd-click internally and Ctrl-click externally

--- Mac/hammerspoon/init.lua
+++ Mac/hammerspoon/init.lua
@@ -1,3 +1,4 @@
 hs.autoLaunch(true)
 
 require("terminal_copy")
+require("vscode_browser")
--- Mac/install/vscode.sh
+++ Mac/install/vscode.sh
@@ -9,6 +9,7 @@ repo_vscodedir="${repo_vscodedir:A}"
 # ref: https://code.visualstudio.com/docs/configure/settings#_settings-file-locations
 vscode_user_dir="${VSCODE_USER_DIR:-$HOME/Library/Application Support/Code/User}"
 vscode_extensions_dir="${VSCODE_EXTENSIONS_DIR:-$HOME/.vscode/extensions}"
+vscode_argv_path="${VSCODE_ARGV_PATH:-$HOME/.vscode/argv.json}"
 
 source "${script_dir}/utils.sh"
 
@@ -20,11 +21,14 @@ else
     echo
 fi
 
-mkdir -p "${vscode_user_dir}" "${vscode_extensions_dir}"
+mkdir -p "${vscode_user_dir}" "${vscode_extensions_dir}" "${vscode_argv_path:h}"
 
 __install_repo_path "${repo_vscodedir}/settings.json" "${vscode_user_dir}/settings.json" 'VSCode settings.json' link
 __install_repo_path "${repo_vscodedir}/keybindings.json" "${vscode_user_dir}/keybindings.json" 'VSCode keybindings.json' link
 __install_repo_path "${repo_vscodedir}/extensions/smart-terminal-paste" "${vscode_extensions_dir}/smart-terminal-paste" 'Smart Terminal Paste extension' link
+__install_repo_path "${repo_vscodedir}/extensions/browser-click-routing" "${vscode_extensions_dir}/browser-click-routing" 'Browser Click Routing extension' link
+__install_repo_path "${repo_vscodedir}/argv.json" "${vscode_argv_path}" 'VSCode argv.json' link
 
 echo 'Finished VSCode configuration!'
+echo 'For Cmd-click / Ctrl-click URL routing, also run Mac/install/hammerspoon.sh and restart VSCode.'
 echo ''
